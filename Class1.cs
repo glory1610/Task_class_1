f0d325cd272c67cf6178584c4230618b3a525dbc
glory1610 <99683140+glory1610@users.noreply.github.com>
2023-03-06 17:57:14 +0200

    Class Triangle

---
+++ Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vyrobnycha_class
+{
+    class Triangle
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public Triangle()
+        {
+            a = 1.0; b = 1.0; c = 1.0;
+        }
+        public Triangle(double _a, double _b, double _c)
+        {
+            a = _a; b = _b; c = _c;
+        }
+        public override string ToString()
+        {
+            return $"a = {a}  b = {b}  c = {c}";
+        }
+        public double Perimeter()
+        {
+            return this.a + this.b + this.c;
+        }
+        public double Half_Perimeter()
+        {
+            return this.Perimeter() / 2.0;
+        }
+        public double Area()
+        {
+            return Math.Sqrt(this.Half_Perimeter() * (this.Half_Perimeter() - this.a) * (this.Half_Perimeter() - this.b) * (this.Half_Perimeter() - this.c));
+        }
+        public static bool operator>(Triangle left, Triangle right)
+        {
+            return left.Area() > right.Area();
+        }
+        public static bool operator<(Triangle left, Triangle right)
+        {
+            return left.Area() < right.Area();
+        }
+        public static Triangle operator*(Triangle left, double n)
+        {
+            return new Triangle(left.a * n, left.b * n, left.c * n);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace vyrobnycha_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle[] triangles = new Triangle[5];
+            triangles[0] = new Triangle(2.0, 3.0, 4.0);
+            triangles[1] = triangles[0] * 2.0;
+            triangles[2] = new Triangle();
+            triangles[3] = new Triangle(1.5, 2.3, 1.2);
+            triangles[4] = new Triangle(2.6, 3.3, 4.5);
+
+            Console.WriteLine("New triangle\n");
+            Console.Write("Enter side a: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Enter side b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Enter side c: ");
+            double c = double.Parse(Console.ReadLine());
+            Array.Resize(ref triangles, triangles.Length + 1);
+            triangles[triangles.Length - 1] = new Triangle(a, b, c);
+
+            Console.WriteLine("All triangles:");
+            Console.WriteLine("------------------------");
+            foreach (var i in triangles)
+            {
+                Console.WriteLine(" {0}\n P = {1}\n S = {2}", i, i.Perimeter(), i.Area());
+                Console.WriteLine("------------------------");
+            }
+
+            double maxElement = triangles[0].Area();
+            for (int i = 1; i < triangles.Length; ++i)
+            {
+                if (triangles[i].Area() > maxElement)
+                {
+                    maxElement = triangles[i].Area();
+                }
+            }
+            Console.WriteLine($"The largest area S = {maxElement}");
+        }
+    }
+}
